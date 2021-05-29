11699dfb02347969030698fafc7db53094d0452a
Henryk Cwikowski <henryk.cwikowski@gmail.com>
2021-05-29 16:22:11 +0200

    Sync

--- ShowCase/Examples/Controller/ExamplesController.cs
+++ ShowCase/Examples/Controller/ExamplesController.cs
@@ -2,10 +2,6 @@
 using ShowCase.Examples.Logic;
 using ShowCase.Examples.Models;
 using ShowCase.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 
 namespace ShowCase.Controllers
